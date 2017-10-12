fdd0828931461ac2923b29448ef7832995ef7d2f
Mariyaeremina <Mariyaeremina@outlook.com>
2017-10-12 23:41:09 +0500

    Completed

--- java-koans-master/koans/src/intermediate/AboutLocale.java
+++ java-koans-master/koans/src/intermediate/AboutLocale.java
@@ -20,24 +20,24 @@ public class AboutLocale {
         Date date = cal.getTime();
         Locale localeBR = new Locale("pt", "BR"); // portuguese, Brazil
         DateFormat dateformatBR = DateFormat.getDateInstance(DateFormat.FULL, localeBR);
-        assertEquals(dateformatBR.format(date), dateformatBR.format(date));
+        assertEquals(dateformatBR.format(date), "Domingo, 3 de Abril de 2011");
 
         Locale localeJA = new Locale("ja"); // Japan
         DateFormat dateformatJA = DateFormat.getDateInstance(DateFormat.FULL, localeJA);
         // Well if you don't know how to type these characters, try "de", "it" or "us" ;-)
-        assertEquals(dateformatJA.format(date), dateformatJA.format(date));
+        assertEquals(dateformatJA.format(date), "Sunday, April 3, 2011");
     }
 
     @Koan
     public void getCountryInformation() {
         Locale locBR = new Locale("pt", "BR");
         assertEquals(locBR.getDisplayCountry(), locBR.getDisplayCountry());
-        assertEquals(locBR.getDisplayCountry(locBR), locBR.getDisplayCountry(locBR));
+        assertEquals(locBR.getDisplayCountry(locBR), "Brasil");
 
         Locale locCH = new Locale("it", "CH");
         assertEquals(locCH.getDisplayCountry(), locCH.getDisplayCountry());
-        assertEquals(locCH.getDisplayCountry(locCH), locCH.getDisplayCountry(locCH));
-        assertEquals(locCH.getDisplayCountry(new Locale("de", "CH")), locCH.getDisplayCountry(new Locale("de", "CH")));
+        assertEquals(locCH.getDisplayCountry(locCH), "Svizzera");
+        assertEquals(locCH.getDisplayCountry(new Locale("de", "CH")), "Schweiz");
     }
 
     @Koan
@@ -45,6 +45,6 @@ public class AboutLocale {
         float someAmount = 442.23f; // Don't use floats for money in real life. Really. It's a bad idea.
         Locale locBR = new Locale("pt", "BR");
         NumberFormat nf = NumberFormat.getCurrencyInstance(locBR);
-        assertEquals(nf.format(someAmount), nf.format(someAmount));
+        assertEquals(nf.format(someAmount), "R$ 442,23");
     }
 }
